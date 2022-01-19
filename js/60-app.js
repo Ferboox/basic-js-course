c28cc3334e837eb56913e2de8e53bd638ea15a76
Ferboox <ferbargar99@hotmail.com>
2022-01-18 23:36:44 -0600

    60.- Parámetros y argumentos

---
+++ js/60-app.js
@@ -0,0 +1,11 @@
+function sumar(a, b) {
+    console.log(a + b);
+}
+
+sumar(2,3); // 2 y 3 son argumentos reales.
+
+function saludar(nombre, apellido){
+    console.log(`Hola ${nombre} ${apellido}`);
+}
+
+saludar('Fernando', 'Pérez');
