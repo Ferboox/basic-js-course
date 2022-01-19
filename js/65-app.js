d1b55e728f4396a7a7a90c9299018aaa23fb54be
Ferboox <ferbargar99@hotmail.com>
2022-01-18 23:57:43 -0600

    64.- Funciones de flecha

---
+++ js/65-app.js
@@ -0,0 +1,9 @@
+/** CAPITULO 65 **/
+const aprendiendo = function() {
+    console.log('Aprnediendo javascript...');
+}
+
+const aprendiendo2 = () => 'Aprnediendo javascript...';
+
+aprendiendo();
+aprendiendo2();
