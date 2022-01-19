e977406eabc69959983b049a040436e0dfdd5639
Ferboox <ferbargar99@hotmail.com>
2022-01-18 23:31:04 -0600

    59.- Función y Método

---
+++ app.js
@@ -0,0 +1,9 @@
+/** CAPITULO 59 **/
+const capitulo59 = () => {
+    const numero1 = 20;
+    const numero2 = "20";
+
+    console.log( parseInt(numero2) ) //parseInt es una función que convierte una cadena en número.
+    console.log( numero1.toString() ) //toString es un método que convierte un número en una cadena.
+}
+
